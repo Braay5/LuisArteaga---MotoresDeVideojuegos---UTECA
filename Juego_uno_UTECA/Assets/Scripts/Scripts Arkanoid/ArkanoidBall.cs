80d7f6efd49c32ed834a9f91a67b8b3dfc087e39
Braay5 <luis.b.ryan@hotmail.com>
2022-10-05 18:59:19 -0500

    Parcial-1
    Se editaron los scripts de jugador y pelota, aparte de los sprites de enemigos.

---
+++ Juego_uno_UTECA/Assets/Scripts/Scripts Arkanoid/ArkanoidBall.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArkanoidBall : MonoBehaviour
+{
+    [SerializeField] private Vector2 initialVelocity;
+
+    private Rigidbody2D ballRb;
+    private bool isBallMoving;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && !isBallMoving)
+        {
+            ballVelocity();
+        }
+    }
+
+    private void ballVelocity()
+    {
+        transform.parent = null;
+        ballRb.velocity = initialVelocity;
+        isBallMoving = true;
+    }
+}
--- Juego_uno_UTECA/Assets/Scripts/Scripts Arkanoid/ArkanoidPlayer.cs
+++ Juego_uno_UTECA/Assets/Scripts/Scripts Arkanoid/ArkanoidPlayer.cs
@@ -6,10 +6,20 @@ public class ArkanoidPlayer : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
 
+    private float bounds = 6.6f;
+
     // Update is called once per frame
     void Update()
     {
+        Movimiento();
+    }
+
+    private void Movimiento()
+    {
         float moveInput = Input.GetAxisRaw("Horizontal");
-        transform.position += new Vector3(moveInput * moveSpeed * Time.deltaTime, 0f, 0f);
+
+        Vector2 playerPosition = transform.position;
+        playerPosition.x = Mathf.Clamp(playerPosition.x + moveInput * moveSpeed * Time.deltaTime, -bounds, bounds);
+        transform.position = playerPosition;
     }
 }
